d0ec7ee16cad52e2de4a7bb0244b97d77f013e37
fox994 <fox994@gmail.com>
2025-03-16 21:03:56 +0800

    Initial commit: AI Story Game

---
+++ service-worker.js
@@ -0,0 +1,69 @@
+const CACHE_NAME = 'ai-story-v1';
+const CACHE_URLS = [
+    '/',
+    '/index.html',
+    '/story.js',
+    '/manifest.json',
+    '/icons/icon-72x72.png',
+    '/icons/icon-96x96.png',
+    '/icons/icon-128x128.png',
+    '/icons/icon-144x144.png',
+    '/icons/icon-152x152.png',
+    '/icons/icon-192x192.png',
+    '/icons/icon-384x384.png',
+    '/icons/icon-512x512.png',
+    '/sounds/hover.mp3',
+    '/sounds/click.mp3',
+    '/sounds/ambient.mp3',
+    '/sounds/ending.mp3',
+    'https://fonts.googleapis.com/css2?family=Share+Tech+Mono&display=swap'
+];
+
+// 安裝 Service Worker
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(CACHE_URLS);
+            })
+    );
+});
+
+// 激活 Service Worker
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// 處理資源請求
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request)
+                    .then(response => {
+                        if (!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+                        const responseToCache = response.clone();
+                        caches.open(CACHE_NAME)
+                            .then(cache => {
+                                cache.put(event.request, responseToCache);
+                            });
+                        return response;
+                    });
+            })
+    );
+}); 
